9b6f16c724ebc174d176ad35fb20147ef33864b1
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-05 18:49:04 -0300

    :tada: Configuração inicial do projeto.

---
+++ src/test/java/br/org/portalfadesp/servicopagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.portalfadesp.servicopagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
