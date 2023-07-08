f2375d665bd8602d2d66f11b3ad1caced84e9d5a
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-07 21:32:25 -0300

    ♻️ Atualizar classe PagamentoRequest
    - Adicionar anotações de validação para garantir a consistência dos dados de entrada.
    - Incluir anotações @NotNull, @NotBlank e @DecimalMin para validar campos obrigatórios e restrições de valores mínimos.
    - Renomear o atributo "metoPagamento" para "metodoPagamento" para seguir a convenção de nomenclatura.

--- src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/request/PagamentoRequest.java
+++ src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/request/PagamentoRequest.java
@@ -1,14 +1,30 @@
 package br.org.portalfadesp.servicopagamento.domain.pagamento.payload.request;
 
-import br.org.portalfadesp.servicopagamento.domain.pagamento.MetoPagamento;
+import br.org.portalfadesp.servicopagamento.domain.pagamento.MetodoPagamento;
 import java.math.BigDecimal;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class PagamentoRequest {
 
+  @NotNull(message = "Código do débito é obrigatório")
   private Integer codigoDebito;
+
+  @NotBlank(message = "CPF/CNPJ do pagador é obrigatório")
   private String cpfCnpjPagador;
-  private MetoPagamento metoPagamento;
+
+  @NotNull(message = "Método de pagamento é obrigatório")
+  private MetodoPagamento metodoPagamento;
+
   private String numeroCartao;
+
+  @NotNull(message = "Valor do pagamento é obrigatório")
+  @DecimalMin(
+    value = "0.0",
+    inclusive = false,
+    message = "Valor do pagamento deve ser maior que zero"
+  )
   private BigDecimal valorPagamento;
 
   public Integer getCodigoDebito() {
@@ -27,12 +43,12 @@ public class PagamentoRequest {
     this.cpfCnpjPagador = cpfCnpjPagador;
   }
 
-  public MetoPagamento getMetoPagamento() {
-    return metoPagamento;
+  public MetodoPagamento getMetodoPagamento() {
+    return metodoPagamento;
   }
 
-  public void setMetoPagamento(MetoPagamento metoPagamento) {
-    this.metoPagamento = metoPagamento;
+  public void setMetodoPagamento(MetodoPagamento metodoPagamento) {
+    this.metodoPagamento = metodoPagamento;
   }
 
   public String getNumeroCartao() {
