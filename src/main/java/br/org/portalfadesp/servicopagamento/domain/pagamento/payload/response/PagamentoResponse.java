93d04d1836d2cbe19f626ac1453724e77de63d84
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-08 20:08:38 -0300

    ♻️ Atualizar PagamentoResponse com anotações do Swagger
    - Adicionadas anotações do Swagger para documentação da API.
    - A anotação `@Schema` é usada para fornecer informações sobre a resposta do PagamentoResponse.
    - Foram adicionadas descrições e exemplos para cada campo do PagamentoResponse.
    - A anotação `@Schema` permite definir a descrição, exemplo e formato dos campos.

--- src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/response/PagamentoResponse.java
+++ src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/response/PagamentoResponse.java
@@ -3,21 +3,32 @@ package br.org.portalfadesp.servicopagamento.domain.pagamento.payload.response;
 import br.org.portalfadesp.servicopagamento.domain.pagamento.MetodoPagamento;
 import br.org.portalfadesp.servicopagamento.domain.pagamento.StatusPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Schema(description = "Resposta de um pagamento")
 public class PagamentoResponse {
 
+  @Schema(description = "ID do pagamento", example = "1")
   private Long id;
 
+  @Schema(description = "Código do débito", example = "12345")
   private Integer codigoDebito;
+
+  @Schema(description = "CPF/CNPJ do pagador", example = "12345678901")
   private String cpfCnpjPagador;
 
+  @Schema(description = "Método de pagamento")
   private MetodoPagamento metodoPagamento;
 
+  @Schema(description = "Valor do pagamento", example = "100.00")
   private BigDecimal valorPagamento;
+
+  @Schema(description = "Status do pagamento", example = "PROCESSADO_COM_SUCESSO")
   private StatusPagamento statusPagamento;
 
+  @Schema(description = "Data do pagamento", pattern = "dd/MM/yyyy HH:mm:ss", example = "01/01/2022 00:00:00")
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
   private LocalDateTime dataPagamento;
 
