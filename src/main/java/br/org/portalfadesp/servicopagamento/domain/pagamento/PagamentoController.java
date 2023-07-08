0e15fa6245003f29a57ff0a847f920d798021531
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-08 20:09:37 -0300

    📝 Atualizar PagamentoController com anotações do Swagger
    - Adicionadas anotações do Swagger para documentação da API.
    - A anotação `@Tag` é usada para agrupar as operações relacionadas aos pagamentos.
    - Foram adicionadas anotações `@Operation` para cada operação do controller, fornecendo informações detalhadas sobre a operação, incluindo descrição, respostas esperadas e parâmetros.
    - A anotação `@ApiResponse` é usada para definir as respostas esperadas de cada operação, incluindo códigos de resposta, descrição e conteúdo.
    - A anotação `@Parameter` é usada para fornecer informações detalhadas sobre os parâmetros das operações, como descrição, exemplo e requisitos.
    - A anotação `@Schema` é usada para definir o esquema das respostas e parâmetros, fornecendo informações como descrição, exemplo e formato.
    - A anotação `@Content` é usada para especificar o conteúdo das respostas, definindo o esquema do corpo da resposta.
    - Foi adicionada a classe `ErrorResponse` como conteúdo para as respostas de erro.
    - Essas anotações melhoram a documentação da API e facilitam o entendimento e uso por parte dos usuários.

--- src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/PagamentoController.java
+++ src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/PagamentoController.java
@@ -1,7 +1,14 @@
 package br.org.portalfadesp.servicopagamento.domain.pagamento;
 
+import br.org.portalfadesp.servicopagamento.domain.pagamento.exception.ErrorResponse;
 import br.org.portalfadesp.servicopagamento.domain.pagamento.payload.request.PagamentoRequest;
 import br.org.portalfadesp.servicopagamento.domain.pagamento.payload.response.PagamentoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import java.util.List;
 import javax.validation.Valid;
@@ -18,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pagamentos")
+@Tag(name = "Pagamentos", description = "API para operações de pagamentos")
 public class PagamentoController {
 
   private final PagamentoService pagamentoService;
@@ -27,6 +35,34 @@ public class PagamentoController {
   }
 
   @PostMapping
+  @Operation(
+    summary = "Criar um novo pagamento",
+    description = "Endpoint para receber o pagamento",
+    operationId = "receberPagamento",
+    responses = {
+      @ApiResponse(
+        responseCode = "201",
+        description = "Pagamento recebido com sucesso",
+        content = @Content(
+          schema = @Schema(implementation = PagamentoResponse.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "400",
+        description = "Pagamento inválido",
+        content = @Content(
+          schema = @Schema(implementation = ErrorResponse.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "404",
+        description = "Pagamento não encontrado",
+        content = @Content(
+          schema = @Schema(implementation = ErrorResponse.class)
+        )
+      ),
+    }
+  )
   public ResponseEntity<PagamentoResponse> receberPagamento(
     @Valid @RequestBody PagamentoRequest pagamentoRequest
   ) {
@@ -39,9 +75,45 @@ public class PagamentoController {
   }
 
   @PatchMapping("/{pagamentoId}/status")
+  @Operation(
+    summary = "Atualizar o status de um pagamento",
+    description = "Endpoint para atualizar o status de um pagamento",
+    operationId = "atualizarStatusPagamento",
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "Pagamento atualizado com sucesso",
+        content = @Content(
+          schema = @Schema(implementation = PagamentoResponse.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "404",
+        description = "Pagamento não encontrado",
+        content = @Content(
+          schema = @Schema(implementation = ErrorResponse.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "409",
+        description = "Conflito no status de pagamento",
+        content = @Content(
+          schema = @Schema(implementation = ErrorResponse.class)
+        )
+      ),
+    }
+  )
   public ResponseEntity<PagamentoResponse> atualizarStatusPagamento(
-    @PathVariable Long pagamentoId,
-    @RequestParam StatusPagamento novoStatus
+    @PathVariable @Parameter(
+      name = "pagamentoId",
+      example = "1",
+      required = true
+    ) Long pagamentoId,
+    @RequestParam @Parameter(
+      name = "novoStatus",
+      description = "Novo status do pagamento",
+      required = true
+    ) StatusPagamento novoStatus
   ) {
     PagamentoResponse pagamentoResponse = pagamentoService.atualizarStatusPagamento(
       pagamentoId,
@@ -51,18 +123,34 @@ public class PagamentoController {
   }
 
   @GetMapping
+  @Operation(
+    summary = "Listar pagamentos com filtros opcionais",
+    description = "Endpoint para listar pagamentos",
+    operationId = "listarPagamentos",
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "Lista de pagamentos",
+        content = @Content(
+          schema = @Schema(implementation = PagamentoResponse.class)
+        )
+      ),
+    }
+  )
   public ResponseEntity<List<PagamentoResponse>> listarPagamentos(
-    @RequestParam(
-      value = "codigoDebito",
-      required = false
+    @RequestParam(value = "codigoDebito", required = false) @Parameter(
+      name = "codigoDebito",
+      description = "Código do débito",
+      example = "12345"
     ) Integer codigoDebito,
-    @RequestParam(
-      value = "cpfCnpjPagador",
-      required = false
+    @RequestParam(value = "cpfCnpjPagador", required = false) @Parameter(
+      name = "cpfCnpjPagador",
+      description = "CPF ou CNPJ do pagador",
+      example = "12345678901"
     ) String cpfCnpjPagador,
-    @RequestParam(
-      value = "statusPagamento",
-      required = false
+    @RequestParam(value = "statusPagamento", required = false) @Parameter(
+      name = "statusPagamento",
+      description = "Status do pagamento"
     ) StatusPagamento statusPagamento
   ) {
     List<PagamentoResponse> pagamentos = pagamentoService.listarPagamentosComFiltros(
@@ -74,7 +162,32 @@ public class PagamentoController {
   }
 
   @DeleteMapping("/{pagamentoId}")
-  public ResponseEntity<Void> deletarPagamento(@PathVariable Long pagamentoId) {
+  @Operation(
+    summary = "Deletar um pagamento",
+    description = "Endpoint para deletar um pagamento",
+    operationId = "deletarPagamento",
+    responses = {
+      @ApiResponse(
+        responseCode = "204",
+        description = "Pagamento deletado com sucesso"
+      ),
+      @ApiResponse(
+        responseCode = "404",
+        description = "Pagamento não encontrado",
+        content = @Content(
+          schema = @Schema(implementation = ErrorResponse.class)
+        )
+      ),
+    }
+  )
+  public ResponseEntity<Void> deletarPagamento(
+    @PathVariable @Parameter(
+      name = "pagamentoId",
+      description = "Id do pagamento",
+      example = "1",
+      required = true
+    ) Long pagamentoId
+  ) {
     pagamentoService.deletarPagamento(pagamentoId);
     return ResponseEntity.noContent().build();
   }
