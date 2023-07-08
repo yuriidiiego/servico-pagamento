b39c5e31bbd7fa545e2208655c8c63e48689a6b4
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-08 20:07:59 -0300

    ♻️ Atualizar PagamentoRequest com anotações do Swagger
    - Adicionadas anotações do Swagger para documentação da API.
    - A anotação `@Schema` é usada para fornecer informações sobre o payload do PagamentoRequest.
    - Foram adicionadas descrições e exemplos para cada campo do PagamentoRequest.
    - A anotação `@Schema` permite definir se um campo é obrigatório ou não.

--- src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/request/PagamentoRequest.java
+++ src/main/java/br/org/portalfadesp/servicopagamento/domain/pagamento/payload/request/PagamentoRequest.java
@@ -1,24 +1,39 @@
 package br.org.portalfadesp.servicopagamento.domain.pagamento.payload.request;
 
 import br.org.portalfadesp.servicopagamento.domain.pagamento.MetodoPagamento;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Schema(description = "Payload para criar um novo pagamento")
 public class PagamentoRequest {
 
+  @Schema(required = true, example = "12345", description = "Código do débito")
   @NotNull(message = "Código do débito é obrigatório")
   private Integer codigoDebito;
 
+  @Schema(
+    required = true,
+    example = "12345678901",
+    description = "CPF/CNPJ do pagador"
+  )
   @NotBlank(message = "CPF/CNPJ do pagador é obrigatório")
   private String cpfCnpjPagador;
 
+  @Schema(required = true, description = "Método de pagamento")
   @NotNull(message = "Método de pagamento é obrigatório")
   private MetodoPagamento metodoPagamento;
 
+  @Schema(example = "1234567890123456", description = "Número do cartão")
   private String numeroCartao;
 
+  @Schema(
+    required = true,
+    example = "100.00",
+    description = "Valor do pagamento"
+  )
   @NotNull(message = "Valor do pagamento é obrigatório")
   @DecimalMin(
     value = "0.0",
